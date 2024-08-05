1a38187ce5d1b3357509757f5c937b481be0efca
Rochiyat <rochiyat@bithealth.co.id>
2024-08-05 16:27:47 +0700

    Add configure for vercel

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import config from "./config/config.js";    // Import the config module
 import timesheetRoute from './routes/timesheet.route.js'; // Import the timesheet route module with extension .js
 
 const app = express();
-const port = config.port || 4000;
+const port = config.port || 3000;
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.use('/api/timesheet', timesheetRoute);
+app.use('/', timesheetRoute);
 
 app.listen(port, () => {
     console.log(`Timesheet app listening at http://localhost:${port}`);
